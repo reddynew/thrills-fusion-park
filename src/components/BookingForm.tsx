ecec158413808093c471564b37d021ecd79a16a5
hanuman reddy <hanumanreddyy@gmail.com>
2025-03-27 17:57:14 +0530

    new

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -450,7 +450,7 @@ const handlenav=()=>{
                   : "bg-secondary/60 text-white border-input"
               }`}
             >
-              UPI Payments
+              UPI Payment
             </button>
             <button
               type="button"
